fea0b43d99290a7791e4eb1dd6da1d44e90d7dd5
Essam <esammosbah1@gmail.com>
2019-03-07 00:50:05 +0200

    paid flavor and test code

---
+++ app/src/main/java/networkAPI/EndPoint.java
@@ -0,0 +1,49 @@
+package networkAPI;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
+
+public class EndPoint extends AsyncTask<onJokeReceived, Void, String> {
+    private static MyApi myApiService = null;
+    private onJokeReceived listener;
+    private String ROOT_URL = "http://192.168.1.35:8085/_ah/api/";
+    @Override
+    protected String doInBackground(onJokeReceived... params) {
+        if(myApiService == null) {
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                    .setRootUrl(ROOT_URL)
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            myApiService = builder.build();
+        }
+
+        listener = params[0];
+
+        try {
+            String name =  myApiService.tellJoke().execute().getData();
+            //if(name!=null)
+            Log.d("hello",name);
+            return name;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        listener.OnJokeReceivedListener(result);
+    }
+}
---
+++ app/src/main/java/networkAPI/onJokeReceived.java
@@ -0,0 +1,6 @@
+package networkAPI;
+
+public interface onJokeReceived {
+    void OnJokeReceivedListener(String joke);
+
+}
