cd1087c4d0fa3411b8d92fe75b387978362271f7
Essam <esammosbah1@gmail.com>
2019-03-07 23:22:48 +0200

    test code modification

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/NotEmptyTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/NotEmptyTest.java
@@ -8,27 +8,39 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import networkAPI.EndPoint;
 import networkAPI.onJokeReceived;
+
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class NotEmptyTest {
+    final CountDownLatch signal = new CountDownLatch(1);
+    String mJoke;
+
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
             MainActivity.class);
 
     @Test
-   public void retrieveValidJokeTest(){
+   public void retrieveValidJokeTest() throws InterruptedException {
         EndPoint endPoint = new EndPoint();
         endPoint.execute(new onJokeReceived() {
             @Override
             public void OnJokeReceivedListener(String joke) {
-                String mJoke = joke;
-                assertNotEquals("", mJoke);
+                 mJoke = joke;
+                signal.countDown();
             }
         });
+       signal.await(10,TimeUnit.SECONDS);
+       assertNotNull("joke is null",mJoke);
+       assertFalse("joke is empty",mJoke.isEmpty());
     }
 }
